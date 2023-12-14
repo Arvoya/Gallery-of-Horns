edcc36cd077c04e2d0ac3c8d514579ea13f25645
Isai Chaidez <isaichaidez@gmail.com>
2023-12-13 23:23:50 -0800

    had error of undefined module. chatGPT suggested setting a 'node' envrionment to 'true'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,22 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true, // For browser globals like 'window'
+    es2020: true,
+    node: true, // This line is added to support Node.js global variables and Node.js scoping.
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: { version: '18.2' }
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
