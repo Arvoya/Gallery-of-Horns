bb47e9c73b8a2df099fded3eb7ae23e8805713fa
Isai Chaidez <isaichaidez@gmail.com>
2023-12-17 23:21:59 -0800

    ran into an naming issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Header from './components/header.jsx'
+import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx'
 import Gallery from './components/Gallery.jsx'
 
