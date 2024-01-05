e911add4d762f96fecae5f14405c5ec58637680d
Isai Chaidez <isaichaidez@gmail.com>
2024-01-04 16:58:24 -0800

    made changes to way user would find beasts based off number of horns thanks to feedback given

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,49 +4,52 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
 class Gallery extends React.Component {
   
+  
   render() {
-  if (this.props.searchedBeast === undefined) {
-    return (
-      <Container >
-        <Row>
-        {this.props.beastData.map((data) => (
-          <Col key={data._id}>
-            <HornedBeast
-            image_url={data.image_url}
-            title={data.title}
-            description={data.description}
-            keyword={data.keyword}
-            horns={data.horns}/>
-            <button onClick={() => {this.props.updateBeastID(data._id); this.props.userClick();}}>More Info</button>
-          </Col>
-          ))}
-        </Row>
-      </Container>)
-      } else if (this.props.searchedBeast > 0) { 
-          let newBeastData = this.props.beastData.filter((data) => data.horns === this.props.searchedBeast)
-          
-          return (
-            <Container>
-              <Row>
-              {newBeastData.map((data) => (
-                <Col key={data._id}>
-                  <HornedBeast
-                  image_url={data.image_url}
-                  title={data.title}
-                  description={data.description}
-                  keyword={data.keyword}
-                  horns={data.horns}/>
-                  <button onClick={() => {this.props.updateBeastID(data._id); this.props.userClick();}}>More Info</button>
-                </Col>
-                ))}
-              </Row>
-            </Container>)
-       }
-      }
-   
-  }
+
+    if (this.props.searchedBeast === undefined) {
+      return (
+        <Container >
+          <Row>
+          {this.props.beastData.map((data) => (
+            <Col key={data._id}>
+              <HornedBeast
+              image_url={data.image_url}
+              title={data.title}
+              description={data.description}
+              keyword={data.keyword}
+              horns={data.horns}/>
+              <button onClick={() => {this.props.updateBeastID(data._id); this.props.userClick();}}>More Info</button>
+            </Col>
+            ))}
+          </Row>
+        </Container>)
+        } else if (this.props.searchedBeast > 0) { 
+            let newBeastData = this.props.beastData.filter((data) => data.horns === this.props.searchedBeast)
+            
+            return (
+              <Container>
+                <Row>
+                {newBeastData.map((data) => (
+                  <Col key={data._id}>
+                    <HornedBeast
+                    image_url={data.image_url}
+                    title={data.title}
+                    description={data.description}
+                    keyword={data.keyword}
+                    horns={data.horns}/>
+                    <button onClick={() => {this.props.updateBeastID(data._id); this.props.userClick();}}>More Info</button>
+                  </Col>
+                  ))}
+                </Row>
+              </Container>)
+        }
+        }
+    
+    }
   
 
 export default Gallery;
